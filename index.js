2c96e2a2af53e1ebdf9685c2004139f6f26edb47
0m3ga13 <barkaoussama13@gmail.com>
2023-07-20 23:44:16 +0100

    Done

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+const scuberGreetingForFeet = (feet) =>
+  feet <= 400
+    ? "This one is on me!"
+    : feet > 400 && feet <= 2000
+    ? "That will be twenty bucks."
+    : feet > 2000 && feet<2500
+    ? "I will gladly take your thirty bucks."
+    : "No can do.";
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+const ternaryCheckCity = (city) => city == "NYC" ? "Ok, sounds good." : "No go.";
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+const switchOnCharmFromTip = (tip) => tip == 'generous' ? "Thank you so much." : tip == 'not as generous' ? 'Thank you.' : "Bye.";
